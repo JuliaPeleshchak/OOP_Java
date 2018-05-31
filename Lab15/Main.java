820c5496e0d2791ad815bf3609abce5ac3275547
JuliaPeleshchak <37551616+JuliaPeleshchak@users.noreply.github.com>
2018-05-31 15:44:46 +0300

    Lab is loaded

---
+++ Lab15/Main.java
@@ -0,0 +1,32 @@
+package ua.lpnuai.oop.peleshchak15;
+
+import com.sun.javaws.util.JfxHelper;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+
+public class Main
+{
+    private JFrame window;
+    public Main()
+    {
+        window = new JFrame("Lab №15");
+        window.setSize(750,500);
+        window.add(new Container());
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+
+    }
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Main();
+            }
+        });
+    }
+
+}
